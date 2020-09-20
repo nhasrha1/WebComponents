e5abb8198ccfef83f536d01743aca57bda15bb9c
Harsha Priyank Nerella <harsha.priyank@rallyhealth.com>
2020-09-20 09:35:52 +0530

    c-3: connectedCallback intro

--- codings/tooltip/first.js
+++ codings/tooltip/first.js
@@ -1,12 +1,16 @@
 class Tooltip extends HTMLElement {
   constructor(){
     super()
+    console.log('in super first');
+  }
+
+  connectedCallback() {
     const tooltipIcon = document.createElement('span')
     tooltipIcon.textContent = ' (?)'
 
     this.appendChild(tooltipIcon)
-    console.log('in super first');
   }
 }
+
 
 customElements.define('ct-tooltip', Tooltip)
