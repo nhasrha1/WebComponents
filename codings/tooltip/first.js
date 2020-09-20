bca97acc8bd52b02aeed073fed95d5fbb36613c9
Harsha Priyank Nerella <harsha.priyank@rallyhealth.com>
2020-09-20 09:59:39 +0530

    c-4: added events on DOM

--- codings/tooltip/first.js
+++ codings/tooltip/first.js
@@ -1,16 +1,26 @@
 class Tooltip extends HTMLElement {
-  constructor(){
-    super()
-    console.log('in super first');
+  constructor() {
+    super();
+    this._tooltipContainer;
   }
 
   connectedCallback() {
-    const tooltipIcon = document.createElement('span')
-    tooltipIcon.textContent = ' (?)'
+    const tooltipIcon = document.createElement('span');
+    tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.appendChild(tooltipIcon);
+  }
 
-    this.appendChild(tooltipIcon)
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = "dummy text";
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
   }
 }
 
-
-customElements.define('ct-tooltip', Tooltip)
+customElements.define('ct-tooltip', Tooltip);
